1587dea1827397e796845c594b42cf9bd95704e3
madu <maduamaral2006@gmail.com>
2023-07-29 13:27:39 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int numero;
+Console.WriteLine("Digite um número");
+Console.WriteLine("Para saber se ele é negativo ou positivo");
+numero = Convert.ToInt32(Console.ReadLine()!);
+if (numero < 0){
+    Console.WriteLine("Este número é negativo");
+}
+else {
+    Console.WriteLine("Seu número é positivo");
+}
